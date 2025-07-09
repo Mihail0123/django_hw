b088d2c9773872df95a5421af79ada4d5c47748d
root <root@DESKTOP-5PG41J9.localdomain>
2025-07-09 14:45:43 +0200

    Add OverdueFilter and colored SubTask status

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
+from django.utils.html import format_html
+from django.utils.timezone import now
+from django.contrib.admin import SimpleListFilter
+
+class OverdueFilter(SimpleListFilter):
+    title = 'Overdue'
+    parameter_name = 'is_overdue'
+
+    def lookups(self, request, model_admin):
+        return (
+        ('yes', 'overdue'),
+        ('no', 'not overdue'),
+        )
+
+    def queryset(self, request, queryset):
+        today = now().date()
+        if self.value() == 'yes':
+            return queryset.filter(status__in=['NEW', 'IN_PROGRESS'], deadline__lt=today)
+        elif self.value() == 'no':
+            return queryset.exclude(status__in=['NEW', 'IN_PROGRESS'], deadline__lt=today)
+        return queryset
 
 class SubTaskInline(admin.TabularInline):
     model = SubTask
@@ -19,9 +40,10 @@ class TaskAdmin(admin.ModelAdmin):
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at')
-    list_filter = ('status', 'deadline', 'created_at', 'task')
+    list_display = ('title', 'colored_status', 'deadline', 'created_at')
+    list_filter = ('status', 'deadline', 'created_at', 'task', OverdueFilter)
     search_fields = ('title', 'description')
+
 
     @admin.action(description='Set status to DONE')
     def mark_as_done(self, request, queryset):
@@ -29,6 +51,17 @@ class SubTaskAdmin(admin.ModelAdmin):
         self.message_user(request, f"{updated} subtask(s) marked as done.")
 
     actions = [mark_as_done]
+
+    def colored_status(self, obj):
+        color_map = {
+            "NEW": 'red',
+            "IN_PROGRESS": "orange",
+            "DONE": "green",
+        }
+        color = color_map.get(obj.status, 'black')
+        return format_html('<span style="color: {};">{}</span>', color, obj.get_status_display())
+
+    colored_status.short_description = 'Status'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
