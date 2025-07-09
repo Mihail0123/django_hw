401bb61f8889a4934397051fc3cc20a163019d34
root <root@DESKTOP-5PG41J9.localdomain>
2025-07-09 14:09:18 +0200

    hw11 done

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,17 +1,34 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
 
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at') # колонки в списке задач
+    list_display = ('short_title', 'status', 'deadline', 'created_at') # колонки в списке задач
     list_filter = ('status', 'deadline', 'created_at') # фильтры справа
     search_fields = ('title', 'description') # поиск
+    inlines = [SubTaskInline]
+
+    def short_title(self, obj):
+        return (obj.title[:10] + '...') if len(obj.title) > 10 else obj.title
+
+    short_title.short_description = 'Short title'
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'created_at')
     list_filter = ('status', 'deadline', 'created_at', 'task')
     search_fields = ('title', 'description')
+
+    @admin.action(description='Set status to DONE')
+    def mark_as_done(self, request, queryset):
+        updated = queryset.update(status='DONE')
+        self.message_user(request, f"{updated} subtask(s) marked as done.")
+
+    actions = [mark_as_done]
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
--- task_manager/models/subtask.py
+++ task_manager/models/subtask.py
@@ -6,7 +6,7 @@ class SubTask(models.Model):
 
     title = models.CharField(max_length=100)
     description = models.TextField()
-    task =models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks') #subtasks ссылается на Task, у которого по-атрибуту можно получить все подзадачи
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks') #subtasks ссылается на Task, у которого по-атрибуту можно получить все подзадачи
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='NEW')
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
