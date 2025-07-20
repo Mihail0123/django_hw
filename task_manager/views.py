3705d0bf10dbe0eddf14c953c1f4b77cfd0ec01b
root <root@DESKTOP-5PG41J9.localdomain>
2025-07-20 18:43:38 +0200

    hw14 complete

--- task_manager/views.py
+++ task_manager/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
 from .models import Task, SubTask
+from .pagionation import SubTaskPagination
 from .serializers import TaskSerializer, SubTaskSerializer, SubTaskCreateSerializer
 
 
@@ -16,11 +17,33 @@ from .serializers import TaskSerializer, SubTaskSerializer, SubTaskCreateSeriali
 @api_view(['GET', 'POST'])
 def task_list_create(request):
     if request.method == 'GET':
-        tasks = Task.objects.all()
+        day_param = request.query_params.get('day')
+
+        if day_param:
+            days_map = {
+                'monday': 0,
+                'tuesday': 1,
+                'wednesday': 2,
+                'thursday': 3,
+                'friday': 4,
+                'saturday': 5,
+                'sunday': 6
+            }
+            day_param = day_param.lower()
+
+            if day_param in days_map:
+                weekday = days_map[day_param]
+                tasks = Task.objects.all()
+                tasks = [task for task in tasks if task.deadline.weekday() == weekday]
+            else:
+                return Response({'error': 'Invalid day name'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            tasks = Task.objects.all()
+
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -56,9 +79,17 @@ def task_stats(request):
 # Список и создание подзадач
 class SubTaskListCreateView(APIView):
     def get(self, request):
-        subtasks = SubTask.objects.all()
-        serializer = SubTaskSerializer(subtasks, many=True)
-        return Response(serializer.data)
+        task_title = request.query_params.get('task')
+        status_param = request.query_params.get('status')
+        subtasks = SubTask.objects.select_related('task').all().order_by('-created_at')
+        if task_title:
+            subtasks =subtasks.filter(task__title__icontains=task_title)
+        if status_param:
+            subtasks = subtasks.filter(status__iexact=status_param)
+        paginator = SubTaskPagination()
+        page = paginator.paginate_queryset(subtasks, request)
+        serializer = SubTaskSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         serializer = SubTaskCreateSerializer(data=request.data)
